eafb16b56b372e4cf594cc1c270a32d91a1986da
negar-m <102906293+negar-m@users.noreply.github.com>
2025-10-18 12:35:36 +0330

    add temperature converter project

---
+++ beginner-projects/temperature-converter/temperature-converter.js
@@ -0,0 +1,47 @@
+﻿let $ = document
+let converter = $.querySelector('#converter')
+let result = $.querySelector('.result')
+let firstValue = $.querySelector('.C')
+let secondValue = $.querySelector('.F')
+let convertButton = $.querySelector('.convertButton')
+let resetButton = $.querySelector('.resetButton')
+let changeButton = $.querySelector('.changeButton')
+
+function convert (){
+    if(converter.value ===''){
+        result.innerHTML = 'incorrect value'
+        result.style.color = 'red'
+    }
+    else {
+        if(firstValue.innerHTML ==='°C'){
+            result.innerHTML = converter.value +'°C To '+((converter.value * 9/5) + 32) +'°F'
+            result.style.color = 'rgb(255, 255, 102)'
+        }
+        else{
+            result.innerHTML = converter.value +'°F To '+((converter.value -32)*5/9).toFixed(2) +'°C'
+            result.style.color = 'rgb(255, 255, 102)'
+        }
+    
+    }
+}
+function reset(){
+    converter.value = ''
+    result.innerHTML = ''
+}
+function swap(){
+    if( firstValue.innerHTML ==='°C'){
+        firstValue.innerHTML ='°F'
+        secondValue.innerHTML ='°C'
+         converter.setAttribute('placeholder', '°F')
+         $.title = '°F to °C'
+        
+    }else{
+        secondValue.innerHTML ='°F'
+        firstValue.innerHTML ='°C'
+        converter.setAttribute('placeholder', '°C')
+        $.title = '°C to °F'
+    }
+}
+convertButton.addEventListener('click', convert)
+resetButton.addEventListener('click',reset)
+changeButton.addEventListener('click',swap)
