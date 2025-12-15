93c50064d066a70d37dfdccd03cd37dc135714bc
negar-m <102906293+negar-m@users.noreply.github.com>
2025-12-15 15:39:27 +0330

    Add files via upload

---
+++ beginner-projects/keyCode/script.js
@@ -0,0 +1,24 @@
+﻿let $ = document
+let locationKey = $.querySelector('#location')
+let keyCode = $.querySelector('#keyCode')
+let key = $.querySelector('#key')
+let which = $.querySelector('#which')
+let code = $.querySelector('#code')
+let title = $.querySelector('title')
+
+document.body.addEventListener('keydown', function (event) {
+	event.preventDefault()
+	starter.style.display = 'none'
+	heading.style.display = 'flex'
+	ascii.style.display = 'flex'
+	infos.style.display = 'flex'
+	
+	key.innerHTML = event.key
+	locationKey.innerHTML = event.location
+	which.innerHTML = event.which
+	code.innerHTML = event.code
+	keyCode.innerHTML = event.keyCode
+
+})
+
+
