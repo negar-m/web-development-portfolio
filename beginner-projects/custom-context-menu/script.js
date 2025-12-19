342980265b8bcd116e355a78b9e14f0838535711
negar-m <102906293+negar-m@users.noreply.github.com>
2025-12-19 19:27:56 +0330

    Add files via upload

---
+++ beginner-projects/custom-context-menu/script.js
@@ -0,0 +1,23 @@
+﻿let contextMenu = document.querySelector('.context-menu')
+
+
+
+function contextHandler(event){
+    event.preventDefault()
+    console.log('contextMenu');
+    contextMenu.style.left = event.pageX+'px'
+    contextMenu.style.top = event.pageY+'px'
+    contextMenu.style.display = 'block'
+    console.log(event);
+}
+function clickHandler(){
+    contextMenu.style.display = 'none'
+}
+function keydownHandler(event){
+    if(event.keyCode ==27){
+        contextMenu.style.display = 'none'
+    }
+}
+document.body.addEventListener('contextmenu',contextHandler)
+document.body.addEventListener('click',clickHandler)
+document.body.addEventListener('keydown',keydownHandler)
